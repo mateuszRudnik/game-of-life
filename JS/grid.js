f7d09625015c3da07655450fddaf16e6bc98d932
MateuszRD <107278128+mateuszRudnik@users.noreply.github.com>
2023-04-22 18:29:27 +0200

    uptade

--- JS/grid.js
+++ JS/grid.js
@@ -1,6 +1,14 @@
-function grid(rows) {
-     const canva = document.querySelector(".canva")
-        for(i=0; i<rows; i++){
+function grid() {
+    const Grid = document.querySelector(':root')
+    const Columns = document.getElementById('rangeColumns').value
+    const Rows = document.getElementById('rangeRows').value
+    const canva = document.querySelector(".canva")
+
+    Grid.style.setProperty('--grid-cols', Columns)
+    Grid.style.setProperty('--grid-rows', Rows)
+
+    const Elements = Columns * Rows
+        for(i=0; i<Elements; i++){
             
             const element = document.createElement("div")
             element.className = "element"
@@ -8,4 +16,12 @@ function grid(rows) {
             console.log(canva)
             canva.appendChild(element)
         }
+    }
+
+function log() {
+    const valueColumns = document.getElementById('rangeColumns').value
+    const valueRows = document.getElementById('rangeRows').value
+    console.log(`
+    Columns: ${valueColumns}
+    Rows: ${valueRows}`)
 }
